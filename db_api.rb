a7fe14a1fbb49788fc72e6ef1b929c2fecf99449
James <syvertsj.jhu@gmail.com>
2011-10-10 00:08:04 -0400

    first try with method_missing

--- db_api.rb
+++ db_api.rb
@@ -5,7 +5,12 @@ require 'yaml'
 
 class DbApi
 
+  @@students = YAML.load_file("university_db.yml")
+
   def self.method_missing(method, *args)
+    puts "a select call" if method =~ /^select*/i 
+    puts "'#{method}' with params: (#{args.join(', ')})"
+    yield "inside method_missing" if block_given?
 
   end
 
