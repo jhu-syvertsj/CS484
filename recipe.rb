b7f66ef92725b39753a65fa0d4f7d1b7f72d2111
unknown <syvertsj@.(none)>
2011-11-03 14:31:48 -0400

    finally paying attention to hw spec

--- recipe.rb
+++ recipe.rb
@@ -5,13 +5,13 @@ class Recipe
   
   include HTTParty  
   base_uri 'http://www.recipepuppy.com'
-  default_params :onlyImages => 1
+  default_params :onlyImages => 1 # (from hw spec)
   format :json
 
   def self.for(keyword)
-    options = { :query => {:title => keyword}, :ingredients => keyword }
+    #get('/api').inspect 
     puts "\nretrieving recipes for #{keyword}: "
-    get('/api', options)
+    get('/api', :query => {:q => keyword})["results"]  # using 'q' from hw spec
   end
 
 
@@ -19,4 +19,5 @@ end
 
 
 puts Recipe.for "chocolate"
+puts
 puts Recipe.for "apple pie"
