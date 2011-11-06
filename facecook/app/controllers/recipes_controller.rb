6ac21242fc694ae111296d6b8ac603bff45c67a9
unknown <syvertsj@.(none)>
2011-11-06 16:27:19 -0500

    added sanitization

--- facecook/app/controllers/recipes_controller.rb
+++ facecook/app/controllers/recipes_controller.rb
@@ -2,5 +2,6 @@ class RecipesController < ApplicationController
   def index
     @keyword = params[:keyword] || "chocolate"
     @recipes = Recipe.for @keyword
+    @time = Time.now
   end
 end
--- facecook/app/helpers/recipes_helper.rb
+++ facecook/app/helpers/recipes_helper.rb
@@ -1,2 +1,5 @@
 module RecipesHelper
+  def formatted_time
+    @time.strftime("%I:%M%p")
+  end
 end
